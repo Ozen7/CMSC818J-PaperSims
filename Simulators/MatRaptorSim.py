58adb9f613fc4335520bd1a9ed8c09c293112e65
ozen <nebilaozer@gmail.com>
2023-11-26 03:26:24 -0500

    11/26/23 - MatRaptor: added CSR to C2SR conversion support. Next is Loading in values to PE, and computing output row by row.

--- Simulators/MatRaptorSim.py
+++ Simulators/MatRaptorSim.py
@@ -1,7 +1,59 @@
 import numpy as np
 from collections import deque
 # Step 1: Convert CSR to C2SR - C2SR basically just makes sure that all the data a PE reads at a given time is useful.
+def csr_to_c2sr(data, indices, indptr, num_channels):
+    # Create arrays to store C2SR format data
+    c2sr_values = [[] for x in range(num_channels)]
+    c2sr_column_ids = [[] for x in range(num_channels)]
+    c2sr_row_length = [[] for x in range(num_channels)]
+    c2sr_row_pointer = [[] for x in range(num_channels)]
 
+    # Loop through each row in CSR format
+    for i in range(len(indptr) - 1):
+        # Get the start and end indices for the current row in CSR format
+        start_idx = indptr[i]
+        end_idx = indptr[i + 1]
+
+        # Get the channel assigned to the current row
+        channel = i % num_channels
+        
+        # Append the row length and row pointer for the current row in C2SR format
+        c2sr_row_length[channel].append(end_idx - start_idx)
+        c2sr_row_pointer[channel].append(len(c2sr_values[channel]))
+
+        # Loop through the non-zero elements of the current row
+        for j in range(start_idx, end_idx):
+            # Append the value and column id for the current element in C2SR format
+            c2sr_values[channel].append(data[j])
+            c2sr_column_ids[channel].append(indices[j])
+        
+        
+        
+
+    return c2sr_values, c2sr_column_ids, c2sr_row_length, c2sr_row_pointer
+
+"""
+# Example usage
+# data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# indices = [0, 2, 4, 1, 3, 4, 0, 2, 3]
+# indptr = [0, 2, 4, 7, 9]
+datacsr = csr_matrix(sparsematrix)
+data = datacsr.data
+indices = datacsr.indices
+indptr = datacsr.indptr
+
+num_channels = 3
+
+c2sr_values, c2sr_column_ids, c2sr_row_length, c2sr_row_pointer = csr_to_c2sr(data, indices, indptr, num_channels)
+
+# Print the results
+assert(len(c2sr_values) == len(c2sr_column_ids))
+
+print("C2SR Values:", c2sr_values)
+print("C2SR Column IDs:", c2sr_column_ids)
+print("C2SR Row Lengths:", c2sr_row_length)
+print("C2SR Row Pointers:", c2sr_row_pointer)
+"""
 # Step 2: Develop functionality of a single PE assigned to each row.
 
 
@@ -64,8 +116,12 @@ class PE:
         c = a * b
         self.merge(c,i,j)
         
+# Step 3: Develop a way to load information in parallel to each PE, splitting based on row. 
+class Loader:
+    
+    def __init__(self,A,B):
         
-pe = PE(50)
+        
 
 # A
 # [1, 0, 2, 3]
@@ -91,20 +147,9 @@ pe.compute(3,1,0,3,3)
 print(pe.queuelist)
 
 
-            
-    
-    
-    
-    
-    
-    
+# Step 4: Add true parallelism to the code
 
-    
-    
-
-# Step 3: add multiple Processing elements, and optimize.
-
-# Step 4: add cycle counters as well as memory bandwidth counters
+# Step 5: add cycle counters as well as memory bandwidth counters
 
 
 
